959ebde390f46870aa2691f45c5862aac22dffce
helmatrindade <helmacaires28@gmail.com>
2022-10-14 17:58:33 -0300

    Requisito 3

---
+++ script.js
@@ -0,0 +1,13 @@
+const email = document.querySelector('#email');
+const senha = document.querySelector('#senha');
+const botao = document.querySelector('#botao-senha');
+
+function validaFormulario(event) {
+  event.preventDefault();
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+botao.addEventListener('click', validaFormulario);
