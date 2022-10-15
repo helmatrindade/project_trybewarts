f35c5bc9d780d8cbdc72d674387b48a802486db1
Flavia Pereira <agendaflaviapereira@gmail.com>
2022-10-15 12:08:09 -0300

    Requisito 17

--- script.js
+++ script.js
@@ -4,22 +4,21 @@ const botao = document.querySelector('#botao-senha');
 
 function validaFormulario(event) {
   event.preventDefault();
-  if (email.value === 'tryber@agreement.disabled = true;teste.com' && senha.value === '123456') {
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
   }
-}
+};
 botao.addEventListener('click', validaFormulario);
 
 const send = document.getElementById('submit-btn');
 const agreement = document.getElementById('agreement');
 agreement.disabled = true;
+
 agreement.addEventListener('click', function sendButton () {
- if(agreement.checked === true ) {
+ if(agreement.checked === true) {
   agreement.disabled = false;
-  } else {
-} 
-};
-sendButton();
+  }
+});
 
